421e555b53e5d38ceaedaddd40f0c21e3bb343f5
Yashasvi Chaurasia <yashasvi12977@gmail.com>
2022-09-19 00:06:54 +0530

    Automated commits @ Mon 19 Sep 2022 00:06:54 IST
    Signed-off-by: Yashasvi Chaurasia <yashasvi12977@gmail.com>

--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+void prt(int n,int m){
+    int k=m+n;
+    printf("%i %i \n",m,n);
+    // return k;
+}
 int main(){
-    printf("Hei");
-    printf("tetstign automerge fail#1");
+    printf("Hei \nhiiii\n");
+    prt(4,5);
+   
 }
--- C/up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-name=$(date);
-echo $name;
-git add .
-git commit -S -sm "Demo commit @ $name bluffed"
-git push origin main
-echo "Success ";
